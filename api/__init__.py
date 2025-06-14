ce3516583ec231e6c2c400d492c45ac51f1588af
s-maarouf <maarouf.salahedd@gmail.com>
2025-06-14 19:14:31 +0100

    Removed unused files

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import register_bp, login_bp
-from .courses import courses_bp
-from .labs import labs_bp
-from .users import users_bp, profile_bp
-from .subjects import subjects_bp, modules_bp
-from .classes import class_bp
