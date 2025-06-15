262e7cd5057c09b1aa6e0502450f996e96ef77c9
s-maarouf <maarouf.salahedd@gmail.com>
2025-06-15 13:47:48 +0100

    Updated config for deploiment

--- src/app.py
+++ src/app.py
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         Base.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- src/config.py
+++ src/config.py
@@ -7,5 +7,5 @@ Base = SQLAlchemy()
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.getenv(
-    'DATABASE_URL', 'mysql://smaarouf:S%40l%40HM%40%40r0uF@localhost/cloudstack')
+    'DATABASE_URL', 'mysql://root:Mugiwara311428@localhost/cloudstack')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
